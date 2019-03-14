fd5b86f025ad321df567fd06125f719d2af2910e
Rudy Ravelin <rudy@noukod.com>
2019-03-14 20:17:07 +0000

    Done.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+
+//this file is optional
+//you can use this to paste in js code and have it run when index.html is loaded
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
